43f0a4153b4bc0f76ab29b3279e81d3a537939d4
Timo Schlüßler <timo@schluessler.org>
2022-11-03 22:02:32 +0100

    FIX : clippy + cargofmt

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-use mysql::*;
 use mysql::prelude::*;
-use std::sync::mpsc::{sync_channel, SyncSender, TryRecvError};
-use std::net::{TcpListener, TcpStream, SocketAddr};
-use std::io::{ErrorKind, Write};
+use mysql::*;
 use std::collections::VecDeque;
-use std::time::{Instant, Duration};
-use popol;
+use std::io::{ErrorKind, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::mpsc::{sync_channel, SyncSender, TryRecvError};
+use std::time::{Duration, Instant};
 
 type Result<R, E> = std::result::Result<R, E>;
 
 fn parse_args() -> Result<(String, u16, u32, Duration), Box<dyn std::error::Error>> {
 	let mut args = std::env::args().skip(1);
 	let mysql_url = match args.next() {
-		Some(env) if env == "env" =>
-			match std::env::var("PROXYSQL_BINLOG_READER_MYSQL_URL") {
-				Err(_) => Err("missing environment variable PROXYSQL_BINLOG_READER_MYSQL_URL")?,
-				Ok(e) => e,
-			},
+		Some(env) if env == "env" => match std::env::var("PROXYSQL_BINLOG_READER_MYSQL_URL") {
+			Err(_) => Err("missing environment variable PROXYSQL_BINLOG_READER_MYSQL_URL")?,
+			Ok(e) => e,
+		},
 		Some(url) => url,
 		None => Err("missing mandatory first parameter")?,
 	};
@@ -28,14 +26,13 @@ fn parse_args() -> Result<(String, u16, u32, Duration), Box<dyn std::error::Erro
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
 	let (mysql_url, listen_port, server_id, simulated_delay) = match parse_args() {
 		Err(e) => {
 			println!("Error parsing arguments: {}", e);
 			println!("\nSyntax: {} {{\"env\" | mysql connection url}} [port to listen on for proxysql connections] [our slave server_id] [simulated delay in ms]", std::env::args().next().unwrap());
 			println!("\texample mysql connection url: mysql://replication_user:replication_pw@localhost:3306/?prefer_socket=false");
 			return Ok(());
-		},
+		}
 		Ok(p) => p,
 	};
 
@@ -59,7 +56,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			println!("error receiving binary log: {:?}", e);
 		}
 	});
-
 
 	// server
 
@@ -86,8 +82,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		// Wait for something to happen on our sources.
 		if let Some((until, _)) = delay_list.front() {
 			match sources.wait_timeout(&mut events, until.duration_since(Instant::now())) {
-				Ok(()) => {},
-				Err(err) if err.kind() == std::io::ErrorKind::TimedOut => {},
+				Ok(()) => {}
+				Err(err) if err.kind() == std::io::ErrorKind::TimedOut => {}
 				Err(err) => Err(err)?,
 			};
 		} else {
@@ -112,23 +108,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 						Err(e) => return Err(e)?,
 					};
 					// Register the new peer using the `Peer` variant of `Source`.
-					sources.register(
-						Source::Peer(addr),
-						&conn,
-						popol::interest::READ
-					);
-					conn.write(format!("ST={}:{}-{}\n", last_gtid.to_uuid(), last_gtid.id, last_gtid.id).as_bytes())?;
+					sources.register(Source::Peer(addr), &conn, popol::interest::READ);
+					conn.write_all(
+						format!(
+							"ST={}:{}-{}\n",
+							last_gtid.to_uuid(),
+							last_gtid.id,
+							last_gtid.id
+						)
+						.as_bytes(),
+					)?;
 					clients.push(conn);
 
 					println!("got {} clients", clients.len());
-				}
+				},
 				Source::Peer(addr) => {
 					println!("{} has data to be read. event: {:?}", addr, event);
-					clients.retain(|c| { println!("peer is {:?} {:?}", c.peer_addr(), addr); c.peer_addr().ok().map(|a| &a == addr) == Some(false) });
+					clients.retain(|c| {
+						println!("peer is {:?} {:?}", c.peer_addr(), addr);
+						c.peer_addr().ok().map(|a| &a == addr) == Some(false)
+					});
 					sources.unregister(key);
 				}
 				Source::Waker => {
-					if let Err(_) = popol::Waker::reset(event.source) {
+					if popol::Waker::reset(event.source).is_err() {
 						break 'a; // waker dropped - mysql thread finished
 					}
 					match rx.try_recv() {
@@ -138,7 +141,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 							} else {
 								delay_list.push_back((Instant::now() + simulated_delay, gtid));
 							}
-						},
+						}
 						Err(TryRecvError::Empty) => (),
 						Err(TryRecvError::Disconnected) => break 'a,
 					};
@@ -146,20 +149,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			}
 		}
 	}
-	
+
 	mysql_thread.join().unwrap();
 
 	Ok(())
 }
 
-fn handle_gtid(gtid: Gtid, last: &mut Gtid, clients: &[TcpStream]) -> Result<(), Box<dyn std::error::Error>> {
+fn handle_gtid(
+	gtid: Gtid,
+	last: &mut Gtid,
+	clients: &[TcpStream],
+) -> Result<(), Box<dyn std::error::Error>> {
 	let msg = if gtid.domain == last.domain && gtid.server == last.server {
 		format!("I2={}\n", gtid.id)
 	} else {
 		format!("I1={}:{}\n", gtid.to_uuid_without_dashes(), gtid.id)
 	};
 	for mut client in clients.iter() {
-		client.write(msg.as_bytes())?;
+		client.write_all(msg.as_bytes())?;
 	}
 	*last = gtid;
 
@@ -180,7 +187,6 @@ impl Gtid {
 	fn to_uuid_without_dashes(&self) -> UuidWithoutDashes<'_> {
 		UuidWithoutDashes { gtid: self }
 	}
-
 }
 
 impl std::str::FromStr for Gtid {
@@ -193,7 +199,7 @@ impl std::str::FromStr for Gtid {
 			Ok(Gtid {
 				domain: domain.parse().map_err(map_err)?,
 				server: server.parse().map_err(map_err)?,
-				id: id.parse().map_err(map_err)?
+				id: id.parse().map_err(map_err)?,
 			})
 		} else {
 			Err("invalid GTID syntax")
@@ -214,7 +220,11 @@ struct Uuid<'a> {
 
 impl<'a> Display for Uuid<'a> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-		write!(f, "{:0>8X}-0000-0000-0000-{:0>12X}", self.gtid.domain, self.gtid.server)
+		write!(
+			f,
+			"{:0>8X}-0000-0000-0000-{:0>12X}",
+			self.gtid.domain, self.gtid.server
+		)
 	}
 }
 
@@ -224,34 +234,43 @@ struct UuidWithoutDashes<'a> {
 
 impl<'a> Display for UuidWithoutDashes<'a> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-		write!(f, "{:0>8X}000000000000{:0>12X}", self.gtid.domain, self.gtid.server)
+		write!(
+			f,
+			"{:0>8X}000000000000{:0>12X}",
+			self.gtid.domain, self.gtid.server
+		)
 	}
 }
 
-fn receive_binlog(waker: popol::Waker, tx: SyncSender<Gtid>, url: &str, server_id: u32) -> Result<(), Box<dyn std::error::Error>> {
+fn receive_binlog(
+	waker: popol::Waker,
+	tx: SyncSender<Gtid>,
+	url: &str,
+	server_id: u32,
+) -> Result<(), Box<dyn std::error::Error>> {
 	let mut connection = Conn::new(url)?;
 
 	let mut gtid : Gtid = match connection.query_first::<String, _>("select @@gtid_binlog_pos")?.unwrap() {
-		gtid if gtid.len() == 0 => {
+		gtid if gtid.is_empty() => {
 			println!("WARNING: server reports empty gtid_binlog_pos - probably it hasn't replicated any transaction yet. Using gtid_slave_pos instead.");
 			connection.query_first::<String, _>("select @@gtid_slave_pos")?.unwrap()
 		},
 		gtid => gtid,
 	}.parse()?;
 	println!("starting from: {}", gtid);
-	//let uuid = 
+	//let uuid =
 
 	connection.query_drop("set @mariadb_slave_capability=4")?;
 	connection.query_drop(format!("set @slave_connect_state='{}'", gtid))?;
 	connection.query_drop("set @slave_gtid_strict_mode=1")?;
 
-	let mut binlog = connection.get_binlog_stream(BinlogRequest::new(server_id))?;
+	let binlog = connection.get_binlog_stream(BinlogRequest::new(server_id))?;
 	// .with_flags(BinlogDumpFlags::BINLOG_DUMP_NON_BLOCK))?; <- this closes the connection if it would block :(
 
 	tx.send(gtid.clone())?;
 	waker.wake().unwrap();
 
-	while let Some(entry) = binlog.next() {
+	for entry in binlog {
 		match entry {
 			Ok(entry) => {
 				//println!("type: {:?}, server: {}", entry.header().event_type(), entry.header().server_id());
@@ -262,10 +281,9 @@ fn receive_binlog(waker: popol::Waker, tx: SyncSender<Gtid>, url: &str, server_i
 					tx.send(gtid.clone())?;
 					waker.wake().unwrap();
 				}
-			},
+			}
 			Err(err) => println!("got error {:?}", err),
 		};
 	}
 	Ok(())
 }
-
