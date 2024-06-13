b564bca4596d161cd1e8e0b0926025a70e37bf79
Timo Schlüßler <timo@schluessler.org>
2024-06-13 22:24:37 +0200

    ADD : support for mysql - original proxysql_binlog_reader doesn't support caching_sha2_password auth plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use byteorder::{ByteOrder, BigEndian};
 use mysql::prelude::*;
 use mysql::*;
 use std::collections::VecDeque;
@@ -5,6 +6,7 @@ use std::io::{ErrorKind, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::mpsc::{sync_channel, SyncSender, TryRecvError};
 use std::time::{Duration, Instant};
+use uuid::Uuid;
 
 type Result<R, E> = std::result::Result<R, E>;
 
@@ -51,8 +53,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let (tx, rx) = sync_channel(4);
 	let waker = popol::Waker::new(&mut sources, Source::Waker)?;
 
+	let (tx_initial_gtids, rx_initial_gtids) = sync_channel(1);
+
 	let mysql_thread = std::thread::spawn(move || {
-		if let Err(e) = receive_binlog(waker, tx, &mysql_url, server_id) {
+		if let Err(e) = receive_binlog(waker, tx, tx_initial_gtids, &mysql_url, server_id) {
 			println!("error receiving binary log: {:?}", e);
 		}
 	});
@@ -72,7 +76,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let mut clients = Vec::with_capacity(4);
 
 	let mut delay_list = VecDeque::<(Instant, Gtid)>::new();
-	let mut known_gtids = Gtids::new(rx.recv()?); // await first gtid before accepting any connections
+	let mut known_gtids = Gtids::new(&rx.recv()?); // await first gtid before accepting any connections
+
+	if let Ok(gtids) = rx_initial_gtids.try_recv() {
+		// in this case we can safely ignore the first sent gtid because it's already in the initial_gtids
+		known_gtids = gtids;
+	}
 
 	// bootstrap: ST= uuid:from-to,uuid:from-to (uuid including dashes)
 	// update: I1= uuid:id (uuid without dashes)
@@ -94,7 +103,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 			if until > &Instant::now() {
 				break;
 			}
-			handle_gtid(delay_list.pop_front().unwrap().1, &mut known_gtids, &clients)?;
+			handle_gtid(
+				delay_list.pop_front().unwrap().1,
+				&mut known_gtids,
+				&clients
+			)?;
 		}
 
 		for (key, event) in events.iter() {
@@ -154,12 +167,12 @@ fn handle_gtid(
 	gtids: &mut Gtids,
 	clients: &[TcpStream],
 ) -> Result<(), Box<dyn std::error::Error>> {
-	assert_eq!(gtid.domain, gtids.domain);
 	let msg = if gtids.add(&gtid) {
 		format!("I2={}\n", gtid.id)
 	} else {
-		format!("I1={}:{}\n", gtid.to_uuid_without_dashes(), gtid.id)
+		format!("I1={}:{}\n", gtid.uuid.as_simple(), gtid.id)
 	};
+	//println!("gtids are now {:?}", gtids.servers[0]);
 	for mut client in clients.iter() {
 		client.write_all(msg.as_bytes())?;
 	}
@@ -169,20 +182,22 @@ fn handle_gtid(
 
 #[derive(Debug, Clone)]
 struct Gtid {
-	domain: u32,
-	server: u32,
+	uuid: Uuid,
 	id: u64,
 }
 
 impl Gtid {
-	fn to_uuid(&self) -> Uuid<'_> {
-		Uuid { gtid: self }
+	fn new(uuid: Uuid, id: u64) -> Gtid {
+		Gtid { uuid, id }
 	}
-	fn to_uuid_without_dashes(&self) -> UuidWithoutDashes<'_> {
-		UuidWithoutDashes { gtid: self }
+
+	fn set_server(&mut self, server_id: u32) {
+		use uuid::Bytes;
+		let bytes =
+			unsafe { &mut *(self.uuid.as_bytes() as *const Bytes as *mut Bytes) }; // safe because uuid is &mut - unfortunately uuid has no update or as_bytes_mut() fn's
+		BigEndian::write_u32(&mut bytes[12..16], server_id);
 	}
 }
-
 impl std::str::FromStr for Gtid {
 	type Err = &'static str;
 
@@ -190,9 +205,15 @@ impl std::str::FromStr for Gtid {
 		let map_err = |_| "invalid GTID syntax";
 		let mut split = s.split('-');
 		if let (Some(domain), Some(server), Some(id)) = (split.next(), split.next(), split.next()) {
+			let mut buf = [0; 8];
+			BigEndian::write_u32(&mut buf[4..8], server.parse().map_err(map_err)?);
 			Ok(Gtid {
-				domain: domain.parse().map_err(map_err)?,
-				server: server.parse().map_err(map_err)?,
+				uuid: Uuid::from_fields(
+					domain.parse().map_err(map_err)?,
+					0,
+					0,
+					&buf
+				),
 				id: id.parse().map_err(map_err)?,
 			})
 		} else {
@@ -204,30 +225,28 @@ impl std::str::FromStr for Gtid {
 use std::fmt::{Display, Formatter};
 impl Display for Gtid {
 	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-		write!(f, "{}-{}-{}", self.domain, self.server, self.id)
+		write!(f, "{}:{}", self.uuid, self.id)
 	}
 }
 
+#[derive(Debug)]
 struct Gtids {
-	domain: u32,
 	servers: Vec<GtidsPerServer>,
 }
 
 impl Gtids {
-	fn new(gtid: Gtid) -> Gtids {
+	fn new(gtid: &Gtid) -> Gtids {
 		Gtids {
-			domain: gtid.domain,
-			servers: vec![GtidsPerServer::new(gtid.server, gtid.id)],
+			servers: vec![GtidsPerServer::new(gtid)],
 		}
 	}
 
 	fn add(&mut self, gtid: &Gtid) -> bool {
-		assert_eq!(self.domain, gtid.domain); // domain should never change
 		if let Some((i, server)) = self
 			.servers
 			.iter_mut()
 			.enumerate()
-			.find(|(_, s)| s.server == gtid.server)
+			.find(|(_, s)| s.uuid == gtid.uuid)
 		{
 			server.add(gtid.id);
 			if i == 0 {
@@ -235,44 +254,83 @@ impl Gtids {
 			}
 			self.servers[0..=i].rotate_right(1); // keep latest server at the beginning
 			//self.servers.swap(i, self.servers.len() - 1); // keep latest server at the end
-			return false;
+			false
 		} else {
 			self.servers
-				.insert(0, GtidsPerServer::new(gtid.server, gtid.id));
+				.insert(0, GtidsPerServer::new(gtid));
 			if self.servers.len() > 10 {
 				self.servers.remove(self.servers.len() - 1);
 			}
-			return false;
+			false
 		}
 	}
+
+	// attention: gtids returned by mysql (e.g. @@gtid_executed) are ordered lexically by uuid, not by age as we do it!
+	fn any_gtid(&self) -> Gtid {
+		Gtid::new(self.servers[0].uuid, self.servers[0].id_ranges.last().unwrap().last)
+	}
 }
+
+impl std::str::FromStr for Gtids {
+	type Err = &'static str;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let map_err = |_| "invalid GTIDs syntax";
+		let server_strings = s.split(',').map(|s| s.trim()).filter(|s| !s.is_empty());
+		let mut servers = Vec::with_capacity(s.chars().filter(|c| *c == ',').count());
+		for server_string in server_strings {
+			let mut split = server_string.split(':');
+			if let Some(uuid) = split.next() {
+				let uuid = uuid.parse().map_err(|_| "UUID in GTIDs invalid")?;
+				let mut id_ranges = Vec::with_capacity(server_string.chars().filter(|c| *c == ':').count() - 1);
+				for id_range in split {
+					// TODO what about domains? (currently ignored because we don't use them)
+					let mut split = id_range.split('-');
+					if let (Some(first), Some(last)) = (split.next(), split.next()) {
+						id_ranges.push(IdRange {
+							first: first.parse().map_err(map_err)?,
+							last: last.parse().map_err(map_err)?,
+						});
+					} else {
+						return Err("invalid GTID syntax");
+					}
+				}
+				servers.push(GtidsPerServer {
+					uuid,
+					id_ranges,
+				});
+			} else {
+				return Err("invalid GTID syntax");
+			}
+		}
+		//println!("parsed {} to {:?}", s, servers);
+		Ok(Gtids { servers })
+	}
+}
+
 
 impl Display for Gtids {
 	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
 		for server in self.servers.iter().rev() { // .rev() because latest server is at beginning and we want to send the latest last to proxysql
-			let gtid = Gtid {
-				domain: self.domain,
-				server: server.server,
-				id: 0,
-			};
 			for ids in server.id_ranges.iter() {
-				write!(f, "{}:{}-{},", gtid.to_uuid(), ids.first, ids.last)?;
+				write!(f, "{}:{}-{},", server.uuid, ids.first, ids.last)?;
 			}
 		}
 		Ok(())
 	}
 }
 
+#[derive(Debug)]
 struct GtidsPerServer {
-	server: u32,
+	uuid: Uuid,
 	id_ranges: Vec<IdRange>,
 }
 
 impl GtidsPerServer {
-	fn new(server: u32, id: u64) -> GtidsPerServer {
+	fn new(gtid: &Gtid) -> GtidsPerServer {
 		GtidsPerServer {
-			server,
-			id_ranges: vec![IdRange::new(id)],
+			uuid: gtid.uuid,
+			id_ranges: vec![IdRange::new(gtid.id)],
 		}
 	}
 
@@ -309,6 +367,7 @@ impl GtidsPerServer {
 	}
 }
 
+#[derive(Debug)]
 struct IdRange {
 	first: u64,
 	last: u64,
@@ -323,64 +382,32 @@ impl IdRange {
 	}
 }
 
-struct Uuid<'a> {
-	gtid: &'a Gtid,
-}
-
-impl<'a> Display for Uuid<'a> {
-	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-		write!(
-			f,
-			"{:0>8X}-0000-0000-0000-{:0>12X}",
-			self.gtid.domain, self.gtid.server
-		)
-	}
-}
-
-struct UuidWithoutDashes<'a> {
-	gtid: &'a Gtid,
-}
-
-impl<'a> Display for UuidWithoutDashes<'a> {
-	fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-		write!(
-			f,
-			"{:0>8X}000000000000{:0>12X}",
-			self.gtid.domain, self.gtid.server
-		)
-	}
-}
-
 fn receive_binlog(
 	waker: popol::Waker,
 	tx: SyncSender<Gtid>,
+	tx_initial_gtids: SyncSender<Gtids>,
 	url: &str,
 	server_id: u32,
 ) -> Result<(), Box<dyn std::error::Error>> {
+	/*let mut connection = Conn::new(OptsBuilder::from_opts(Opts::try_from(url)?)
+		.additional_capabilities(mysql::consts::CapabilityFlags::CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA)
+		)?;*/
 	let mut connection = Conn::new(url)?;
 
-	let mut gtid : Gtid = match connection.query_first::<String, _>("select @@gtid_binlog_pos")?.unwrap() {
-		gtid if gtid.is_empty() => {
-			println!("WARNING: server reports empty gtid_binlog_pos - probably it hasn't replicated any transaction yet. Using gtid_slave_pos instead.");
-			connection.query_first::<String, _>("select @@gtid_slave_pos")?.unwrap()
-		},
-		gtid => gtid,
-	}.parse()?;
-	let start_with_logfile = find_binlog_filename(&mut connection, 1024 * 1024 * 10);
+	let is_mariadb = connection
+		.query_first::<String, _>("select @@version")?
+		.unwrap()
+		.to_lowercase()
+		.contains("mariadb");
 
-	let mut request = BinlogRequest::new(server_id);
-	connection.query_drop("set @mariadb_slave_capability=4")?;
-	if let Some(name) = start_with_logfile.as_ref() {
-		println!("starting with logfile {}", name);
-		request = request.with_filename(name.as_bytes()).with_pos(4u64); // logs star at position 4 (after magic bytes)
-	} else {	
-		println!("starting from gtid {}", gtid);
-		connection.query_drop(format!("set @slave_connect_state='{}'", gtid))?;
+	let (mut gtid, request) = if is_mariadb {
+		prepare_binlog_request_mariadb(&mut connection, server_id, &tx)?
+	} else {
+		prepare_binlog_request_oracle(&mut connection, server_id, &tx, tx_initial_gtids)?
+	};
+	waker.wake().unwrap();
 
-		tx.send(gtid.clone())?;
-		waker.wake().unwrap();
-	}
-	connection.query_drop("set @slave_gtid_strict_mode=1")?;
+	//println!("prepared, now requesting binlog");
 
 	let binlog = connection.get_binlog_stream(request)?;
 	// .with_flags(BinlogDumpFlags::BINLOG_DUMP_NON_BLOCK))?; <- this closes the connection if it would block :(
@@ -389,13 +416,28 @@ fn receive_binlog(
 		match entry {
 			Ok(entry) => {
 				//println!("type: {:?}, server: {}", entry.header().event_type(), entry.header().server_id());
-				if entry.header().event_type() == Err(mysql::binlog::UnknownEventType(0xa2)) {
-					gtid.id = u64::from_le_bytes(entry.data()[0..8].try_into().unwrap());
-					gtid.server = entry.header().server_id();
-					//println!("gtid is: {}", gtid);
-					tx.send(gtid.clone())?;
-					waker.wake().unwrap();
-				}
+				match entry.header().event_type() {
+					Err(mysql::binlog::UnknownEventType(0xa2)) => {
+						gtid.id = u64::from_le_bytes(entry.data()[0..8].try_into().unwrap());
+						gtid.set_server(entry.header().server_id());
+						//println!("gtid is: {}", gtid);
+						tx.send(gtid.clone())?;
+						waker.wake().unwrap();
+					},
+					Ok(mysql::binlog::EventType::GTID_EVENT) => {
+						let gtid_event : mysql::binlog::events::GtidEvent = entry.read_event()?;
+						if &gtid_event.sid() != gtid.uuid.as_bytes() {
+							println!("uuid changed from {} to {}", gtid, Gtid::new(Uuid::from_slice(&gtid_event.sid())?, gtid_event.gno()));
+							gtid = Gtid::new(Uuid::from_slice(&gtid_event.sid())?, gtid_event.gno());
+						} else {
+							gtid.id = gtid_event.gno();
+						}
+						//println!("gtid is: {}", gtid);
+						tx.send(gtid.clone())?;
+						waker.wake().unwrap();
+					},
+					_ => {},
+				};
 			}
 			Err(err) => println!("got error {:?}", err),
 		};
@@ -426,3 +468,44 @@ fn find_binlog_filename(connection: &mut Conn, approx_byte_distance: u64) -> Opt
 	first.map(|l| l.name)
 }
 
+fn prepare_binlog_request_mariadb(connection: &mut Conn, server_id: u32, tx: &SyncSender<Gtid>) -> Result<(Gtid, BinlogRequest<'static>), Box<dyn std::error::Error>> {
+	let gtid : Gtid = match connection.query_first::<String, _>("select @@gtid_binlog_pos")?.unwrap() {
+		gtid if gtid.is_empty() => {
+			println!("WARNING: server reports empty gtid_binlog_pos - probably it hasn't replicated any transaction yet. Using gtid_slave_pos instead.");
+			connection.query_first::<String, _>("select @@gtid_slave_pos")?.unwrap()
+		},
+		gtid => gtid,
+	}.parse()?;
+	let start_with_logfile = find_binlog_filename(connection, 1024 * 1024 * 10);
+
+	let mut request = BinlogRequest::new(server_id);
+	connection.query_drop("set @mariadb_slave_capability=4")?;
+	if let Some(name) = start_with_logfile.as_ref() {
+		println!("starting with logfile {}", name);
+		request = request.with_filename(name.clone().into_bytes()).with_pos(4u64); // logs star at position 4 (after magic bytes)
+	} else {
+		println!("starting from gtid {}", gtid);
+		connection.query_drop(format!("set @slave_connect_state='{}'", gtid))?;
+
+		tx.send(gtid.clone())?;
+	}
+	connection.query_drop("set @slave_gtid_strict_mode=1")?;
+
+	Ok((gtid, request))
+}
+
+fn prepare_binlog_request_oracle(connection: &mut Conn, server_id: u32, tx: &SyncSender<Gtid>, tx_initial_gtids: SyncSender<Gtids>) -> Result<(Gtid, BinlogRequest<'static>), Box<dyn std::error::Error>> {
+	let (file, position, _, _, gtids) = connection.query_first::<(String, u64, String, String, String), _>("show binary log status")?.unwrap();
+	let gtids : Gtids = gtids.parse()?;
+
+	println!("starting from file {} at position {}", file, position);
+	let request = BinlogRequest::new(server_id)
+		.with_use_gtid(true)
+		.with_filename(file.into_bytes())
+		.with_pos(position);
+	let any_gtid = gtids.any_gtid();
+	tx_initial_gtids.send(gtids)?;
+	tx.send(any_gtid.clone())?;
+
+	Ok((any_gtid, request))
+}
